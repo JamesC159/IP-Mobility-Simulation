156e62c775795868f34b5cd32de9fe476ee5d9bf
notian <ianjosephbrown@gmail.com>
2018-04-25 09:18:45 -0500

    Still commenting, but this is the near final code.

--- Node/driver2.py
+++ Node/driver2.py
@@ -1,6 +1,6 @@
 from Node import Node
 
 host = '127.0.0.1'
-port = 2525
+port = 2526
 
-node = Node(host, port, True)
+node = Node(host, port, False)
--- Router/RouterListener.py
+++ Router/RouterListener.py
@@ -1,12 +1,9 @@
 import socket
 import threading
 import time
-from socketserver import ThreadingMixIn
 
 
-# Multithreaded Python server : TCP Server Socket Thread Pool
 class RouterListener(threading.Thread):
-    # Multithreaded Python server : TCP Server Socket Program Stub
 
     def __init__(self, TCP_IP, TCP_PORT, rtID, BUFFER_SIZE = 1024):
         super().__init__()
@@ -15,19 +12,28 @@ class RouterListener(threading.Thread):
         self.TCP_PORT = TCP_PORT
         self.BUFFER_SIZE = BUFFER_SIZE
 
-        # NEW PARAMETER: Routers must specify the router ID (IP address within "network")
         # Used by initRouters() in order to not create socket with itself
         self.rtID = rtID
+
+        # Create bound socket to receive connections from Nodes, Home Agents, and other Routers
         self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((self.TCP_IP, self.TCP_PORT))
         self.socket.listen(5)
+
+        # Stores home agent connection information when home agent registered with router
         self.homeAgent = ()
+
+        # Used for determining next IP to allocate
         self.ipCount = 2
+
+        # Stores dictionary mapping for other router IP addresses and their associated sockets
         self.routers = {}
+
+        # Stores dictionary mapping of node IP addresses and their associated sockets
         self.nodes = {}
 
-        print("[+] New server socket thread started for " + self.TCP_IP + ":" + str(TCP_PORT))
+        print("[+] New server socket thread started for " + self.TCP_IP + ":" + str(TCP_PORT) + " with network address of " + self.rtID)
 
         self.start()
 
@@ -36,25 +42,30 @@ class RouterListener(threading.Thread):
         # Sleep for 5 seconds to allow for starting other routers
         time.sleep(5)
 
+        # Set up sockets with other routers
         self.initRouters()
 
         print("Mobile IP Router : Waiting for connections from TCP clients...")
         while True:
             conn, addr = self.socket.accept()
-            # conn.settimeout(60)
             threading.Thread(target=self.clientListen, args=(conn, addr)).start()
 
 
     def clientListen(self, conn, addr):
         """
-        Add support for REGISTER HA to set the home agent for this network
+        Thread to listen for messages sent by clients/home agents and handle them according to their commands
+        :param conn:      socket connection with client
+        :param addr:      sent address of client
+        :return:    boolean
         """
         while True:
             try:
                 data = conn.recv(2048).decode()
                 if data:
+                    # Each field of message delineated by a space
                     splitData = data.split(' ')
 
+                    # All messages must be at least 3 fields long
                     if(len(splitData) < 3):
                         print("Malformed packet received.")
                         continue
@@ -63,38 +74,32 @@ class RouterListener(threading.Thread):
                     dst = splitData[1]
                     payload = splitData[2]
 
-
-
-    # Very close to having home agent reassigned as ip address. Only issue is checking dst for self.TCP_IP
-    # which will be 127.0.0.1. Solution should be to check the fake network address as dst instead of self.TCP_IP.
-    # Need sleep, but I will be back up in a little over 4 hours to finish what you don't get done.
-                    if dst == self.TCP_IP:
+                    # Handles if message intended for router
+                    if dst == self.TCP_IP or dst == self.rtID:
+                        # Allocate new IP address for node
                         if (payload == 'REGISTER'):
-
-                            # Allocate new IP address for node
                             newIP = self.allocateIP(conn, addr, data)
-
-                        elif (payload == 'FOREIGN'):
-                            # Register CoA with home agent
-                            self.registerHA(conn, addr, data)
 
                         # Triggered when the node is a registering HA
                         elif (payload == 'HA'):
                             self.setHA(conn, addr, data, newIP)
 
-                        # Testing only; allows for making sure routers are connecting, will remove before turn in
+                        # Message sent between routers when they establish their connections with each other
                         elif ('ROUTER' in payload):
                             print(payload)
 
                         # Sets the home agent's socket as the route to the home IP of the mobile node
                         elif (payload == 'SETHA'):
-                            print('HA set for ' + dst)
+                            print('HA set for ' + splitData[3])
                             self.nodes[splitData[3]] = conn
 
+                        # Triggered if command/msg type not recognized
                         else:
                             print("Server  data:" + str(data))
                             MESSAGE = 'Not a valid command'
                             conn.send(str.encode(MESSAGE))  # echo
+
+                    # Handles if message is intended for a node
                     else:
                         self.handleMessage(splitData)
 
@@ -141,7 +146,7 @@ class RouterListener(threading.Thread):
         src = packet[0]
         dst = packet[1]
         payload = packet[2]
-        builtPkt = " ".join(packet)
+        builtPkt = packet[0] + ' ' + packet[1] + ' ' + packet[2]
         conn = None
         haIP = None
         print("Source IP: " + src)
@@ -157,9 +162,14 @@ class RouterListener(threading.Thread):
         # Check if destination address is located on this router's network
         if dst in self.nodes:
             print("Destination is on the network")
-            print("Sending packet to " + dst + ": " + builtPkt)
-            conn = self.nodes[dst]
-            conn.send(builtPkt.encode())
+            msg = ''
+            for field in packet:
+                msg += field
+                msg += ' '
+
+            print("Sending packeter to " + dst + ": " + msg)
+            nodeconn = self.nodes[dst]
+            nodeconn.send(msg.encode())
             print("Packet sent")
 
         # Check if destination address is within network of another router and route appropriately
@@ -168,10 +178,12 @@ class RouterListener(threading.Thread):
 
             # Had to make some messages 4 fields long (for registering CoA with Home Agent)
             # Looping through the fields allows for getting all fields rather than narrowing down to 3
+            print("Packet length is " + str(len(packet)))
             msg = ''
             for field in packet:
                 msg += field
                 msg += ' '
+            print("Routing message is " + msg)
             self.routers[routerCheck].send(msg.encode())
         else:
             print("[-] Cannot send packet to destination")
@@ -202,21 +214,6 @@ class RouterListener(threading.Thread):
                 return False
 
         return True
-
-
-    def registerHA(self, conn, addr, data):
-        """
-        Reaches out to home agent of home network to register CoA IP for proper tunneling
-        :param conn: socket descriptor for connection with node
-        :param addr: address of sending node
-        :param data: message sent by client in string format
-        :return: void
-
-        TO BE IMPLEMENTED
-        """
-
-        print("Register HA.")
-        pass
 
     def setHA(self, conn, addr, data, ip):
         """
